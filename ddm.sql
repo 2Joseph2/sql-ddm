9c76ee8210504fc3c63b83319d8924509205dfd0
2Joseph2 <bouslimiyoussef9@gmail.com>
2024-10-23 15:27:16 +0100

    adding file

---
+++ ddm.sql
@@ -0,0 +1,11 @@
+INSERT INTO Product (Product_id, Product_Name, Category, Price)
+VALUES ('P01', 'Samsung Galaxy S20', 'Smartphone', 3299);
+
+INSERT INTO Product (Product_id, Product_Name, Category, Price)
+VALUES ('P02', 'ASUS Notebook', 'PC', 4599);
+
+INSERT INTO Customer (Customer_id, Customer_Name, Customer_Tel)
+VALUES ('C01', 'ALI', 71321009);
+
+INSERT INTO Customer (Customer_id, Customer_Name, Customer_Tel)
+VALUES ('C02', 'ASMA', 77345823);
